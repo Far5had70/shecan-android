82c996a8f1790ee76e410e966718a1e6dd846fc8
Farshad <ferik800@ymail.com>
2025-12-25 13:23:13 +0330

    change app urls in constant

--- app/src/main/java/ir/shecan/core/constant/Constant.java
+++ app/src/main/java/ir/shecan/core/constant/Constant.java
@@ -2,11 +2,11 @@ package ir.shecan.core.constant;
 
 public class Constant {
 
-    public static String BaseAuthRedirect = "https://my.shecan.ir/new-dashboard/auth?token=%s&url=%s";
+    public static String BaseAuthRedirect = "https://my.shecan.ir/panel/auth?token=%s&url=%s";
 
-    public static String TransactionUrlRaw = "https://my.shecan.ir/new-dashboard/transactions";
-    public static String TicketUrlRaw = "https://my.shecan.ir/new-dashboard/support";
-    public static String DomainUrlRaw = "https://my.shecan.ir/new-dashboard/domains";
-    public static String OrderWebPageUrlRaw = "https://my.shecan.ir/new-dashboard/orders";
+    public static String TransactionUrlRaw = "https://my.shecan.ir/panel/transactions";
+    public static String TicketUrlRaw = "https://my.shecan.ir/panel/support";
+    public static String DomainUrlRaw = "https://my.shecan.ir/panel/domains";
+    public static String OrderWebPageUrlRaw = "https://my.shecan.ir/panel/orders";
     public static String PlanUrl = "https://my.shecan.ir/panel";
 }
